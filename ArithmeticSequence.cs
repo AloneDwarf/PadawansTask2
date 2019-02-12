c921ca6c7caa010386b594199d92850b812746bd
Stepanovich Vladislav <stepanovich.98@mail.ru>
2019-02-12 12:11:34 +0300

    third

--- ArithmeticSequence.cs
+++ ArithmeticSequence.cs
@@ -6,7 +6,7 @@ namespace PadawansTask2
     {
         public static int Calculate(int number, int add, int count)
         {
-
+            int k = 0;
             if (number < int.MinValue || add < int.MinValue || count < int.MinValue)
                 throw new OverflowException();
             if (number > int.MaxValue || add > int.MaxValue || count > int.MaxValue)
@@ -16,17 +16,19 @@ namespace PadawansTask2
             int result = 0;
             for (int i = 0; i < count; i++)
             {
-                if ((result += add) > int.MaxValue)
+                k = result;
+                if ((k += number) > int.MaxValue)
                     throw new OverflowException();
-                if ((result -= add) < int.MinValue)
+                if ((k += number) < int.MinValue)
                     throw new OverflowException();
                 result += number;
 
                 for (int j = 0; j < i; j++)
                 {
-                    if ((result += add) > int.MaxValue)
+                    k = result;
+                    if ((k += add) > int.MaxValue)
                         throw new OverflowException();
-                    if ((result -= add) < int.MinValue)
+                    if ((k += add) < int.MinValue)
                         throw new OverflowException();
                     result += add;
                     
